06282629876cb94236383ae75553b31cb571a5cf
MArreola78 <amauricio4467@gmail.com>
2025-02-15 10:39:12 -0600

    Gun Update
    This has Enemy health you need to tag the object to a enemy

--- Assets/Scripts/MA_Scripts/MA_Gun/Bullet.cs
+++ Assets/Scripts/MA_Scripts/MA_Gun/Bullet.cs
@@ -6,7 +6,7 @@ public class Bullet : MonoBehaviour
 {
     // The life span of the bullet
     public float BulletlifeSpan = 3;
-
+    public int Damage = 25;
 
     void Awake()
     {
@@ -15,6 +15,17 @@ public class Bullet : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            E_Health enemyHealth = collision.gameObject.GetComponent<E_Health>();
+            if (enemyHealth != null)
+            {
+                enemyHealth.DamageOnEnemy(Damage);
+            }
+        }
+
         Destroy(gameObject);
     }
+
 }
--- Assets/Scripts/MA_Scripts/MA_Gun/Shooting.cs
+++ Assets/Scripts/MA_Scripts/MA_Gun/Shooting.cs
@@ -4,25 +4,74 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
+    // Gun shoot and speed
     public Transform BulletSpawn;
     public GameObject BulletPrefab;
     public float bulletSpeed = 20;
     public float shootingDelay = 2f; // delay when firing
     private bool canShoot = true;
 
+    // Ammo and Reload 
+    public int maxAmmo = 20;
+    public int currentAmmo;
+    public float reloadTime = 2f;
+    public bool isreload = false;
+
+    // camera Zoom
+    public Camera playerC;
+    public float zoom = 10f;
+    private float normalZoom;
+
+    void Start()
+    {
+        currentAmmo = maxAmmo;
+        normalZoom = playerC.fieldOfView;
+    }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0) && canShoot)
+        if (isreload) return;
+
+        // To shoot 
+        if (Input.GetMouseButton(0) && canShoot && currentAmmo > 0)
         {
             Shoot();
         }
-    }
 
+        // If out then no more bullet
+        else if (Input.GetMouseButton(0) && canShoot && currentAmmo <= 0)
+        {
+            Debug.Log("Out of ammo");
+        }
+
+        // if R is press to reload
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(Reload());
+        }
+
+        // The Right mouse to zoom the camera
+        if (Input.GetMouseButtonDown(1))
+        {
+            playerC.fieldOfView = zoom;
+        }
+
+        else if (Input.GetMouseButtonUp(1))
+        {
+            playerC.fieldOfView = normalZoom;
+        }
+
+    }
     void Shoot()
     {
+        // it move to where the player is facing
         var bullet = Instantiate(BulletPrefab, BulletSpawn.position, BulletSpawn.rotation);
+
+        // This the firerate
         bullet.GetComponent<Rigidbody>().velocity = BulletSpawn.forward * bulletSpeed;
+
+        currentAmmo--;
+        // it the cooldown to shoot
         StartCoroutine(ShootingCooldown());
     }
 
@@ -32,4 +81,18 @@ public class Shooting : MonoBehaviour
         yield return new WaitForSeconds(shootingDelay);
         canShoot = true;
     }
+    // make reload and amount of ammo
+
+    IEnumerator Reload()
+    {
+        isreload = true;
+        Debug.Log("Reloading...");
+        yield return new WaitForSeconds(reloadTime);
+        currentAmmo = maxAmmo;
+        isreload = false;
+        Debug.Log("Reloaded");
+
+    }
+  
+
 }
