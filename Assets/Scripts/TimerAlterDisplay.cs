6db5c0c3da08c0107c65348e3d704f39aa00e0a2
Hoxatron <derpman8000@gmail.com>
2025-02-18 11:55:00 -0600

    Sync with main, Comment Timer Script, Create On-Rails scene
    Matched with main, commented the timer script, and created an empty scene for onrails.

--- Assets/Scripts/TimerAlterDisplay.cs
+++ Assets/Scripts/TimerAlterDisplay.cs
@@ -6,17 +6,20 @@ using UnityEngine.SceneManagement;
 
 public class TimerAlterDisplay : MonoBehaviour
 {
+    [Tooltip("Time limit in seconds.")]
     public float timeLimit = 10f;
-    private float curTimeRemaining;
+    private float curTimeRemaining; // Internal counter for time remaining
+    [Tooltip("When is the timer going to end?")]
     public float endTime = 0f;
-    public bool timerRunning = false;
-    public TextMeshProUGUI timerText;
+    public bool timerRunning = false; // Is the timer actually ticking? Must be triggered from an object.
+    private bool isVisible = true; // Unused as of now.
+    public TextMeshProUGUI timerText; // The textmeshpro object that will hold the timer text
 
     // Start is called before the first frame update
     void Start()
     {
-        timerRunning = false;
-        curTimeRemaining = timeLimit;
+        timerRunning = false; // Timer has to be triggered manually in-game
+        curTimeRemaining = timeLimit; // Update internal counter to match max time limit
     }
     // Update is called once per frame
     void Update()
@@ -30,10 +33,10 @@ public class TimerAlterDisplay : MonoBehaviour
             }
             else
             {
-                timerRunning = false;
+                timerRunning = false; // timer doesn't run when it's out
                 curTimeRemaining = 0;
                 timerText.text = string.Format("00:00");
-                SceneManager.LoadScene("Scene_SubTimeout");
+                SceneManager.LoadScene("Scene_SubTimeout"); // Send them to gameover screen
             }
         }
     }
@@ -42,6 +45,6 @@ public class TimerAlterDisplay : MonoBehaviour
         timeToDisplay += 1;
         float minutes = Mathf.FloorToInt(timeToDisplay / 60); //2
         float seconds = Mathf.FloorToInt(timeToDisplay % 60); //10
-        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds); // Format timer text to be good.
     }
 }
