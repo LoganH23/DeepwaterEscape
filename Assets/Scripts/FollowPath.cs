2ed65f0a91bc919c452d1a0480153460cf55c3a2
ABBush2001 <alex.bush@my.utsa.edu>
2025-02-20 13:43:18 -0600

    Began creating bite mechanic pathing

---
+++ Assets/Scripts/FollowPath.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+    Node[] PathNode;
+    public GameObject enemy;
+    public float moveSpeed;
+    float timer;
+    int currentNode;
+    static Vector3 currentPositionHolder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PathNode = GetComponentsInChildren<Node>();
+        CheckNode();
+    }
+
+    void CheckNode()
+    {
+        timer = 0;
+        currentPositionHolder = PathNode[currentNode].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(currentNode);
+        timer += Time.deltaTime * moveSpeed;
+        if (enemy.transform.position != currentPositionHolder)
+        {
+            enemy.transform.position = Vector3.Lerp(enemy.transform.position, currentPositionHolder, timer);
+        }
+        else
+        {
+            if(currentNode < PathNode.Length - 1)
+            {
+                currentNode++;
+                CheckNode();
+            }
+            else if(currentNode == PathNode.Length - 1)
+            {
+                currentNode = 0;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PathManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathManager : MonoBehaviour
+{
+    [SerializeField] GameObject path1;
+    [SerializeField] GameObject path2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(switchPaths());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator switchPaths()
+    {
+        yield return new WaitForSeconds(Random.Range(5, 10));
+        path1.SetActive(false);
+        path2.SetActive(true);
+        yield return new WaitForSeconds(3);
+        path1.SetActive(true);
+        path2.SetActive(false);
+    }
+}
