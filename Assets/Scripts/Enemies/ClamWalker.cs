0a3ba4d00cf1b0db7ea77e99070c2913fddaf8cb
Hoxatron <derpman8000@gmail.com>
2025-02-27 19:37:55 -0600

    Basic Clam Enemy
    Skeleton of clam and navmesh

--- Assets/Scripts/Enemies/ClamWalker.cs
+++ Assets/Scripts/Enemies/ClamWalker.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
+
+/*
+ * This script handles the clam 
+*/
 
 public class Clam_Walker : MonoBehaviour
 {
+    private bool isAggro = false;
+    public NavMeshAgent clamNavAgent;
+    public Transform navTarget;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,14 @@ public class Clam_Walker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        clamNavAgent.destination = navTarget.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isAggro = true;
+        }
     }
 }
